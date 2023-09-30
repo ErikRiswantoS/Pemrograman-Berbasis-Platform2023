90700e19153f5590872cbc10bf3c6ca633c1321e
ErikRiswantoS <114674061+ErikRiswantoS@users.noreply.github.com>
2023-09-30 11:34:22 +0700

    Add files via upload

---
+++ Var.js
@@ -0,0 +1,5 @@
+const firstName = "Erik Riswanto"
+const lastName = " Saputra"
+const age = 22
+const isMarried = false
+console.log ("Nama AKU", [firstName] + [lastName], "umurku",age, "perkawinanku", isMarried)
